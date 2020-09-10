ef194bc4172bd6d354868ffe7e9a1c8406e8aa62
Liu Xiangqian <xiangqian_liu@163.com>
2020-09-10 23:07:45 +0800

    node-02

---
+++ 正课/20200910/20200910+刘向前+nodejs-02/index.js
@@ -0,0 +1,18 @@
+/*
+ * @Descripttion:
+ * @version:
+ * @Author: 刘向前
+ * @Date: 2020-09-10 22:55:44
+ * @LastEditors: 刘向前
+ * @LastEditTime: 2020-09-10 23:04:20
+ */
+
+// ================
+// 暗号：koa
+// ================
+
+const koa = require('koa')
+const KoaRouter = require('@koa/router')
+const Static = require('koa-static-cache')
+const fs = require('fs')
+const path = require('path')
