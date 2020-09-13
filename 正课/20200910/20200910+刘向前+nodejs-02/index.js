52cf8bdbedcb8d18114b82d635a8a2c6ab056b99
Liu Xiangqian <xiangqian_liu@163.com>
2020-09-13 08:39:34 +0800

    node-02

--- 正课/20200910/20200910+刘向前+nodejs-02/index.js
+++ 正课/20200910/20200910+刘向前+nodejs-02/index.js
@@ -4,15 +4,36 @@
  * @Author: 刘向前
  * @Date: 2020-09-10 22:55:44
  * @LastEditors: 刘向前
- * @LastEditTime: 2020-09-10 23:04:20
+ * @LastEditTime: 2020-09-12 09:23:36
  */
 
 // ================
 // 暗号：koa
 // ================
 
-const koa = require('koa')
+const Koa = require('Koa')
 const KoaRouter = require('@koa/router')
-const Static = require('koa-static-cache')
 const fs = require('fs')
-const path = require('path')
+const staticCache = require('koa-static-cache')
+
+const app = new Koa()
+const router = new KoaRouter()
+
+app.use(
+  staticCache({
+    prefix: '/static',
+    dir: __dirname + '/public',
+    gzip: true,
+    dynamic: true
+  })
+)
+app.use(router.routes())
+
+router.get('/', ctx => {
+  ctx.type = 'html'
+  ctx.body = fs.readFileSync('public/index.html').toString()
+})
+
+app.listen('8888', () => {
+  console.log('the server is running on port 8888')
+})
