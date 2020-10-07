19bbd84b6e8920dabb198bb8e09e16803aff61f1
Liu Xiangqian <xiangqian_liu@163.com>
2020-10-07 21:24:45 +0800

    v1.0.0

--- demo/ts-demo/dist/index.js
+++ demo/ts-demo/dist/index.js
@@ -1,10 +1,51 @@
 "use strict";
 /*
- * @Descripttion:
+ * @Descripttion: 扑克牌练习
  * @version:
  * @Author: 刘向前
  * @Date: 2020-10-07 20:20:25
  * @LastEditors: 刘向前
- * @LastEditTime: 2020-10-07 20:22:00
+ * @LastEditTime: 2020-10-07 21:06:49
  */
-console.log('hello world');
+// 创建一副扑克牌
+function createDeck() {
+    let deckArr = [];
+    for (let i = 1; i <= 13; i++) {
+        deckArr.push({
+            color: '♠',
+            number: i
+        });
+        deckArr.push({
+            color: '♣',
+            number: i
+        });
+        deckArr.push({
+            color: '♥',
+            number: i
+        });
+        deckArr.push({
+            color: '♦',
+            number: i
+        });
+    }
+    return deckArr;
+}
+// 打印扑克牌
+function printDeck(deck) {
+    deck.forEach(item => {
+        if (item.number <= 10) {
+            console.log(item.color + item.number);
+        }
+        else if (item.number === 11) {
+            console.log(item.color + 'J');
+        }
+        else if (item.number === 12) {
+            console.log(item.color + 'Q');
+        }
+        else if (item.number === 13) {
+            console.log(item.color + 'K');
+        }
+    });
+}
+const deck = createDeck();
+printDeck(deck);
--- demo/ts-demo/src/index.ts
+++ demo/ts-demo/src/index.ts
@@ -4,5 +4,54 @@
  * @Author: 刘向前
  * @Date: 2020-10-07 20:20:25
  * @LastEditors: 刘向前
- * @LastEditTime: 2020-10-07 20:32:13
+ * @LastEditTime: 2020-10-07 21:06:49
  */
+
+type Deck = NormalDeck[]
+type NormalDeck = {
+  color: Color
+  number: number
+}
+type Color = '♥' | '♣' | '♠' | '♦'
+// 创建一副扑克牌
+function createDeck(): Deck {
+  let deckArr: NormalDeck[] = []
+  for (let i = 1; i <= 13; i++) {
+    deckArr.push({
+      color: '♠',
+      number: i
+    })
+    deckArr.push({
+      color: '♣',
+      number: i
+    })
+    deckArr.push({
+      color: '♥',
+      number: i
+    })
+    deckArr.push({
+      color: '♦',
+      number: i
+    })
+  }
+
+  return deckArr
+}
+
+// 打印扑克牌
+function printDeck(deck: Deck) {
+  deck.forEach(item => {
+    if (item.number <= 10) {
+      console.log(item.color + item.number)
+    } else if (item.number === 11) {
+      console.log(item.color + 'J')
+    } else if (item.number === 12) {
+      console.log(item.color + 'Q')
+    } else if (item.number === 13) {
+      console.log(item.color + 'K')
+    }
+  })
+}
+
+const deck = createDeck()
+printDeck(deck)
