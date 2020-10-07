377a1482648e7c022c16e88d80b487dabdca42ee
Liu Xiangqian <xiangqian_liu@163.com>
2020-10-07 20:32:54 +0800

    项目初始化

---
+++ demo/ts-demo/dist/index.js
@@ -0,0 +1,10 @@
+"use strict";
+/*
+ * @Descripttion:
+ * @version:
+ * @Author: 刘向前
+ * @Date: 2020-10-07 20:20:25
+ * @LastEditors: 刘向前
+ * @LastEditTime: 2020-10-07 20:22:00
+ */
+console.log('hello world');
---
+++ demo/ts-demo/src/index.ts
@@ -0,0 +1,8 @@
+/*
+ * @Descripttion: 扑克牌练习
+ * @version:
+ * @Author: 刘向前
+ * @Date: 2020-10-07 20:20:25
+ * @LastEditors: 刘向前
+ * @LastEditTime: 2020-10-07 20:32:13
+ */
