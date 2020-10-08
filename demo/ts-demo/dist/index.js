e67dbd6798a8b7d21e8cd404a6b82b057e72a1cf
Liu Xiangqian <xiangqian_liu@163.com>
2020-10-08 11:07:30 +0800

    使用枚举，优化代码

--- demo/ts-demo/dist/index.js
+++ demo/ts-demo/dist/index.js
@@ -5,46 +5,49 @@
  * @Author: 刘向前
  * @Date: 2020-10-07 20:20:25
  * @LastEditors: 刘向前
- * @LastEditTime: 2020-10-07 21:06:49
+ * @LastEditTime: 2020-10-08 11:03:07
  */
+// type Color = '♥' | '♣' | '♠' | '♦'
+var Color;
+(function (Color) {
+    Color["heart"] = "\u2665";
+    Color["spade"] = "\u2660";
+    Color["club"] = "\u2663";
+    Color["diamond"] = "\u2666";
+})(Color || (Color = {}));
+var Mark;
+(function (Mark) {
+    Mark["A"] = "A";
+    Mark["two"] = "2";
+    Mark["three"] = "3";
+    Mark["four"] = "4";
+    Mark["five"] = "5";
+    Mark["six"] = "6";
+    Mark["seven"] = "7";
+    Mark["eight"] = "8";
+    Mark["nine"] = "9";
+    Mark["ten"] = "10";
+    Mark["eleven"] = "J";
+    Mark["twelve"] = "Q";
+    Mark["king"] = "K";
+})(Mark || (Mark = {}));
 // 创建一副扑克牌
 function createDeck() {
     let deckArr = [];
-    for (let i = 1; i <= 13; i++) {
-        deckArr.push({
-            color: '♠',
-            number: i
+    Object.values(Mark).forEach(mark => {
+        Object.values(Color).forEach(color => {
+            deckArr.push({
+                color,
+                mark
+            });
         });
-        deckArr.push({
-            color: '♣',
-            number: i
-        });
-        deckArr.push({
-            color: '♥',
-            number: i
-        });
-        deckArr.push({
-            color: '♦',
-            number: i
-        });
-    }
+    });
     return deckArr;
 }
 // 打印扑克牌
 function printDeck(deck) {
     deck.forEach(item => {
-        if (item.number <= 10) {
-            console.log(item.color + item.number);
-        }
-        else if (item.number === 11) {
-            console.log(item.color + 'J');
-        }
-        else if (item.number === 12) {
-            console.log(item.color + 'Q');
-        }
-        else if (item.number === 13) {
-            console.log(item.color + 'K');
-        }
+        console.log(item.color + item.mark);
     });
 }
 const deck = createDeck();
--- demo/ts-demo/src/index.ts
+++ demo/ts-demo/src/index.ts
@@ -4,52 +4,55 @@
  * @Author: 刘向前
  * @Date: 2020-10-07 20:20:25
  * @LastEditors: 刘向前
- * @LastEditTime: 2020-10-07 21:06:49
+ * @LastEditTime: 2020-10-08 11:03:07
  */
 
 type Deck = NormalDeck[]
 type NormalDeck = {
   color: Color
-  number: number
+  mark: Mark
 }
-type Color = '♥' | '♣' | '♠' | '♦'
+// type Color = '♥' | '♣' | '♠' | '♦'
+enum Color {
+  heart = '♥',
+  spade = '♠',
+  club = '♣',
+  diamond = '♦'
+}
+enum Mark {
+  A = 'A',
+  two = '2',
+  three = '3',
+  four = '4',
+  five = '5',
+  six = '6',
+  seven = '7',
+  eight = '8',
+  nine = '9',
+  ten = '10',
+  eleven = 'J',
+  twelve = 'Q',
+  king = 'K'
+}
 // 创建一副扑克牌
 function createDeck(): Deck {
   let deckArr: NormalDeck[] = []
-  for (let i = 1; i <= 13; i++) {
-    deckArr.push({
-      color: '♠',
-      number: i
-    })
-    deckArr.push({
-      color: '♣',
-      number: i
-    })
-    deckArr.push({
-      color: '♥',
-      number: i
-    })
-    deckArr.push({
-      color: '♦',
-      number: i
-    })
-  }
 
+  Object.values(Mark).forEach(mark => {
+    Object.values(Color).forEach(color => {
+      deckArr.push({
+        color,
+        mark
+      })
+    })
+  })
   return deckArr
 }
 
 // 打印扑克牌
 function printDeck(deck: Deck) {
   deck.forEach(item => {
-    if (item.number <= 10) {
-      console.log(item.color + item.number)
-    } else if (item.number === 11) {
-      console.log(item.color + 'J')
-    } else if (item.number === 12) {
-      console.log(item.color + 'Q')
-    } else if (item.number === 13) {
-      console.log(item.color + 'K')
-    }
+    console.log(item.color + item.mark)
   })
 }
 
