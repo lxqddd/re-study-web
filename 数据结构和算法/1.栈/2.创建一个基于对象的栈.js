a85d7296b1295cf10718304b036083bec726e1d6
Liu Xiangqian <xiangqian_liu@163.com>
2020-11-01 21:33:59 +0800

    给对象形式的栈添加toString方法

--- 数据结构和算法/1.栈/2.创建一个基于对象的栈.js
+++ 数据结构和算法/1.栈/2.创建一个基于对象的栈.js
@@ -1,7 +1,7 @@
 /*
  * @Author       : 刘向前
  * @Date         : 2020-10-28 22:22:53
- * @LastEditTime : 2020-10-28 22:34:09
+ * @LastEditTime : 2020-11-01 21:20:19
  * @LastEditors  : Please set LastEditors
  * @Description  : In User Settings Edit
  * @FilePath     : \re-study-web\数据结构和算法\1.栈\2.创建一个基于对象的栈.js
@@ -67,12 +67,29 @@ class Stack {
   size() {
     return this.count
   }
+  /**
+   * @description: 将栈中的值以字符串的形式输出
+   * @return {string}
+   */
+  toString() {
+    let str = ''
+    const temp = []
+    if (!this.count) {
+      return (str = '')
+    }
+    for (let i = 0; i < this.count; i++) {
+      temp.push(this.items[i])
+    }
+    str = temp.join(',')
+    return str
+  }
 }
 
 const stack = new Stack()
 stack.push('hello')
 stack.push('world')
 stack.push('!')
+console.log(stack.toString())
 stack.pop()
 console.log(stack.peek())
 console.log(stack.size())
