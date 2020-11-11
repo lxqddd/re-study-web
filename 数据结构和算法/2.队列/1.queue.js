3a342855d31bfdf0649dfe91b6bf58e57e2761b7
Liu Xiangqian <xiangqian_liu@163.com>
2020-11-11 23:54:40 +0800

    nothing

--- 数据结构和算法/2.队列/1.queue.js
+++ 数据结构和算法/2.队列/1.queue.js
@@ -1,16 +1,8 @@
 /*
  * @Author       : your name
  * @Date         : 2020-11-08 22:34:08
- * @LastEditTime : 2020-11-08 22:50:59
+ * @LastEditTime : 2020-11-11 22:16:18
  * @LastEditors  : Please set LastEditors
- * @Description  : In User Settings Edit
- * @FilePath     : \re-study-web\数据结构和算法\2.队列\1.queue.js
- */
-/*
- * @Author       : your name
- * @Date         : 2020-11-08 22:34:08
- * @LastEditTime : 2020-11-08 22:36:44
- * @LastEditors  : your name
  * @Description  : In User Settings Edit
  * @FilePath     : \re-study-web\数据结构和算法\2.队列\1.queue.js
  */
@@ -20,6 +12,10 @@ class Queue {
     this.lowestCount = 0
     this.items = {}
   }
-
+  /**
+   * @description:向队列中添加一个元素
+   * @param {*} item 要添加的元素
+   * @return {*}
+   */
   enQueue(item) {}
 }
