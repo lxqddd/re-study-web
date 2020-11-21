884ba05311be0d380ba3eccf384b0ebb6255c929
Liu Xiangqian <xiangqian_liu@163.com>
2020-11-21 11:58:51 +0800

    react router

--- 正课/20201117/20201117+刘向前+react05/homework/src/index.js
+++ 正课/20201117/20201117+刘向前+react05/homework/src/index.js
@@ -1,11 +1,4 @@
-/*
- * @Author       : your name
- * @Date         : 2020-11-18 21:45:19
- * @LastEditTime : 2020-11-21 10:20:45
- * @LastEditors  : Please set LastEditors
- * @Description  : In User Settings Edit
- * @FilePath     : \re-study-web\正课\20201117\20201117+刘向前+react05\homework\src\index.js
- */
+// 今晚上星 ====================
 import ReactDOM from 'react-dom'
 import App from './App'
 import { BrowserRouter } from 'react-router-dom'
--- 正课/20201117/20201117+刘向前+react05/homework/src/store.js
+++ 正课/20201117/20201117+刘向前+react05/homework/src/store.js
@@ -1,26 +1,3 @@
-// import { createStore, combineReducers, applyMiddleware } from 'redux'
-// import thunk from 'redux-thunk'
-// const page = (page = '1', action) => {
-//   console.log(action)
-//   // if (action.type === 'SET_PAGE') {
-//   //   return page
-//   // }
-//   switch (action.type) {
-//     case 'SET_PAGE':
-//       console.log(page)
-//       return page
-//   }
-//   return page
-// }
-
-// const store = createStore(
-//   combineReducers({
-//     page
-//   }),
-//   applyMiddleware(thunk)
-// )
-// export default store
-
 import { createStore } from 'redux'
 const reducer = (
   state = {
