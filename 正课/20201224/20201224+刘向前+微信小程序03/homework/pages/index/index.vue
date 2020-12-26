7e095aca4537c9b1889804468ea37a9df8d45d04
lxq <xiangqian_liu@163.com>
2020-12-26 19:33:25 +0800

    uni-app

---
+++ 正课/20201224/20201224+刘向前+微信小程序03/homework/pages/index/index.vue
@@ -0,0 +1,50 @@
+<template>
+    <view class="wrap">
+        <camera device-position="back" flash="off" @error="error" class="camera"></camera>
+        <button type="primary" @click="takePhoto">拍照</button>
+        <view>预览</view>
+        <image mode="widthFix" :src="src"></image>
+    </view>
+	<!-- 暗号：点积 ============================== -->
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				src:""
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+			takePhoto() {
+				const ctx = uni.createCameraContext();
+				ctx.takePhoto({
+					quality: 'high',
+					success: (res) => {
+						this.src = res.tempImagePath
+					}
+				});
+			},
+			error(e) {
+				console.log(e.detail);
+			}
+		}
+	}
+</script>
+
+<style>
+	.wrap {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		height: 100%;
+	}
+	.camera {
+		width: 300rpx;
+		height: 300rpx;
+	}
+</style>
