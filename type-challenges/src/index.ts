6c74dae9e9a628d55ec135af7c8aaa36e838b774
向阳 <xiangqian_liu@163.com>
2022-04-27 10:38:57 +0800

    feat: FilterByValueType

--- type-challenges/src/index.ts
+++ type-challenges/src/index.ts
@@ -230,5 +230,19 @@ type ToRequired<T> = {
 type resToRequired = ToRequired<ToPartial<Obj>>
 type resToRequired2 = Required<ToPartial<Obj>>
 
+// FilterByValueType
+type FilterByValueType<Obj extends Record<string, any>, ValueType> = {
+  [
+    Key in keyof Obj
+      as ValueType extends Obj[Key]
+        ? Key : never
+  ]: Obj[Key]
+}
+type resFilterByValueType = FilterByValueType<{
+  name: string;
+  age: number;
+  hobby: string[];
+}, number | string>
+
 
 
